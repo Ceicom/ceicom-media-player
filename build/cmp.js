4297e1ad97f10bd6be11a5bef6cad4f2e545b506
Emanoel Queiroz <emanoelqueiroz@gmail.com>
2019-04-23 09:05:52 -0300

    implemented youtube video player

--- build/cmp.js
+++ build/cmp.js
@@ -1,5 +1,9 @@
 "use strict";
 
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; var ownKeys = Object.keys(source); if (typeof Object.getOwnPropertySymbols === 'function') { ownKeys = ownKeys.concat(Object.getOwnPropertySymbols(source).filter(function (sym) { return Object.getOwnPropertyDescriptor(source, sym).enumerable; })); } ownKeys.forEach(function (key) { _defineProperty(target, key, source[key]); }); } return target; }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
@@ -51,7 +55,7 @@ function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _d
           this._currentMusic = 0;
         }
 
-        $(document).trigger('cmp-music-end', [this._currentMusic, error]);
+        $(document).trigger('cmp-media-end', [this._currentMusic, error]);
 
         this._makePlayer();
       }
@@ -190,13 +194,202 @@ function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _d
     return CeicomMediaPlayer;
   }();
 
+  var CeicomVideoPlayer =
+  /*#__PURE__*/
+  function () {
+    function CeicomVideoPlayer(videos) {
+      var options = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
+
+      _classCallCheck(this, CeicomVideoPlayer);
+
+      this._player = {};
+      this._videos = videos;
+      this.options = options;
+      this._actualVideo = 0;
+      this._isPlaying = false;
+      this._fakePlayer = $('.cmp-player');
+      this._$playBtn = this._fakePlayer.find('.play-btn');
+      this._$progressBar = this._fakePlayer.find('.progress-bar');
+      this._$time = this._fakePlayer.find('.time');
+      this._$volumeWrapper = this._fakePlayer.find('.volume-wrapper');
+      this._$volumeInput = this._fakePlayer.find('.volume-input');
+      this._$expandBtn = this._fakePlayer.find('.expand-button');
+      this.init();
+    }
+
+    _createClass(CeicomVideoPlayer, [{
+      key: "init",
+      value: function init() {
+        var _this4 = this;
+
+        if (!this._videos.length) {
+          $(this._fakePlayer).html('<span class="error-msg">Nenhuma música detectada na lista atual.</span>');
+          return;
+        }
+
+        var tag = document.createElement('script');
+        var firstScriptTag = document.getElementsByTagName('script')[0];
+        tag.src = 'https://www.youtube.com/iframe_api';
+        firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+        window.onYouTubeIframeAPIReady = function () {
+          // https://developers.google.com/youtube/player_parameters#Parameters
+          var options = _objectSpread({
+            width: $('body').width(),
+            height: 600,
+            playerVars: {
+              autoplay: 0,
+              cc_load_policy: 0,
+              controls: 0,
+              disablekb: 1,
+              enablejsapi: 1,
+              iv_load_policy: 3,
+              modestbranding: 1,
+              rel: 0,
+              showinfo: 0
+            },
+            events: {
+              onReady: onPlayerReady,
+              onStateChange: onPlayerStateChange,
+              onError: onPlayerError
+            }
+          }, _this4.options);
+
+          _this4._player = new YT.Player('cvp-player', options);
+
+          _this4._initFakePlayerActions();
+        };
+
+        var onPlayerReady = function onPlayerReady() {
+          var videoIds = _this4._videos.map(function (item) {
+            return _this4._validaYoutube(item.link);
+          });
+
+          _this4._player.loadPlaylist(videoIds);
+
+          _this4._player.setLoop(true);
+
+          _this4._isPlaying = true;
+          setInterval(function () {
+            _this4._updateTime(_this4._player.getCurrentTime(), _this4._player.getDuration());
+
+            _this4._updateProgressBar(_this4._player.getCurrentTime(), _this4._player.getDuration());
+          }, 1000);
+        };
+
+        var onPlayerStateChange = function onPlayerStateChange(event) {
+          // Play/Pause Video
+          _this4._isPlaying = true;
+
+          _this4._fakePlayer.addClass('playing');
+
+          if (event.data === 2) {
+            _this4._isPlaying = false;
+
+            _this4._fakePlayer.removeClass('playing');
+          } // Verifica se foi alterado o video
+
+
+          if (_this4._player.getPlaylistIndex() !== _this4._actualVideo) {
+            _this4._actualVideo = _this4._player.getPlaylistIndex();
+
+            _this4._videoChange();
+          }
+        };
+
+        var onPlayerError = function onPlayerError() {
+          _this4._videoChange(true);
+
+          _this4._player.nextVideo();
+        };
+      }
+    }, {
+      key: "_initFakePlayerActions",
+      value: function _initFakePlayerActions() {
+        var _this5 = this;
+
+        this._$playBtn.on('click', function () {
+          return _this5._isPlaying ? _this5._player.pauseVideo() : _this5._player.playVideo();
+        });
+
+        this._$volumeWrapper.on('click', function (e) {
+          if (!$(e.target).hasClass('volume-wrapper')) return;
+
+          _this5._$volumeWrapper.toggleClass('active');
+        });
+
+        this._$volumeInput.on('input', function () {
+          return _this5.setVolume(_this5._$volumeInput.val() / 10);
+        });
+
+        this._$expandBtn.on('click', function () {
+          var ele = _this5._fakePlayer.parent()[0];
+
+          if (document.fullscreen || document.fullscreenElement || document.mozFullScreenElement || document.webkitFullscreenElement || document.msFullscreenElement) {
+            if (document.exitFullscreen) document.exitFullscreen();else if (document.msExitFullscreen) document.msExitFullscreen();else if (document.mozCancelFullScreen) document.mozCancelFullScreen();else if (ele.webkitExitFullscreen) document.webkitExitFullscreen();
+            return;
+          }
+
+          if (ele.requestFullscreen) ele.requestFullscreen();else if (ele.msRequestFullscreen) ele.msRequestFullscreen();else if (ele.mozRequestFullScreen) ele.mozRequestFullScreen();else if (ele.webkitRequestFullscreen) ele.webkitRequestFullscreen();
+        });
+      }
+    }, {
+      key: "getVolume",
+      value: function getVolume() {
+        return this._player.getVolume();
+      }
+    }, {
+      key: "setVolume",
+      value: function setVolume(value) {
+        this._player.setVolume(value * 100);
+      }
+    }, {
+      key: "_validaYoutube",
+      value: function _validaYoutube(link) {
+        var regex = /(?:youtube\.com\/\S*(?:(?:\/e(?:mbed))?\/|watch\?(?:\S*?&?v\=))|youtu\.be\/)([a-zA-Z0-9_-]{6,11})/g;
+        var match = regex.exec(link);
+        return match && match[1].length === 11 ? match[1] : false;
+      }
+    }, {
+      key: "_videoChange",
+      value: function _videoChange(error) {
+        $(document).trigger('cmp-media-end', [this._player.getPlaylistIndex(), error]);
+      }
+    }, {
+      key: "_updateProgressBar",
+      value: function _updateProgressBar(currentTime, musicDuration) {
+        this._$progressBar.css('transform', "scaleX(".concat(Math.ceil(currentTime) / Math.ceil(musicDuration), ")"));
+      }
+    }, {
+      key: "_updateTime",
+      value: function _updateTime(currentTime, musicDuration) {
+        var extraZero = function extraZero(n) {
+          return n < 10 ? '0' + n : n;
+        };
+
+        var formatedTime = function formatedTime(time) {
+          var s = parseInt(Math.ceil(time) % 60);
+          var m = parseInt(Math.ceil(time) / 60 % 60);
+          return extraZero(m) + ':' + extraZero(s);
+        };
+
+        this._$time.text(formatedTime(currentTime) + ' / ' + formatedTime(musicDuration));
+      }
+    }]);
+
+    return CeicomVideoPlayer;
+  }();
+
   var CeicomMediaList =
   /*#__PURE__*/
   function () {
-    function CeicomMediaList(musics) {
+    function CeicomMediaList(mediaList) {
+      var isVideo = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : false;
+
       _classCallCheck(this, CeicomMediaList);
 
-      this._musics = musics;
+      this._mediaList = mediaList;
+      this.isVideo = isVideo;
       this._wrapper = $('.cml-music-list');
       this.init();
     }
@@ -204,23 +397,23 @@ function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _d
     _createClass(CeicomMediaList, [{
       key: "init",
       value: function init() {
-        var _this4 = this;
+        var _this6 = this;
 
-        if (!this._musics.length) return;
+        if (!this._mediaList.length) return;
 
-        var html = this._musics.map(function (item, index) {
+        var html = this._mediaList.map(function (item, index) {
           if (item.typemedia === 'comercial' || item.typemedia === 'vinheta') return;
-          return "<li class=\"music-item ".concat(index === 0 ? 'active' : '', "\" data-music=\"").concat(index, "\">").concat(item.track, "</li>");
+          return "<li class=\"music-item ".concat(index === 0 ? 'active' : '', "\" \n                            data-music=\"").concat(index, "\">").concat(_this6.isVideo ? item.title : item.track, "</li>");
         }).join('');
 
-        $(document).on('cmp-music-end', function (e, index, error) {
-          if (error) _this4._wrapper.find('.music-item.active').addClass('error');
+        $(document).on('cmp-media-end', function (e, index, error) {
+          if (error) _this6._wrapper.find('.music-item.active').addClass('error');
 
-          _this4._wrapper.find('.music-item').removeClass('active');
+          _this6._wrapper.find('.music-item').removeClass('active');
 
-          _this4._wrapper.find(".music-item[data-music=".concat(index, "]")).addClass('active');
+          _this6._wrapper.find(".music-item[data-music=".concat(index, "]")).addClass('active');
 
-          _this4._scrollToMusic();
+          _this6._scrollToMusic();
         });
 
         this._wrapper.html(html);
@@ -251,9 +444,11 @@ function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _d
 
   if (typeof exports !== "undefined") {
     exports.CeicomMediaPlayer = CeicomMediaPlayer;
+    exports.CeicomVideoPlayer = CeicomVideoPlayer;
     exports.CeicomMediaList = CeicomMediaList;
   } else {
     window.CeicomMediaPlayer = CeicomMediaPlayer;
+    window.CeicomVideoPlayer = CeicomVideoPlayer;
     window.CeicomMediaList = CeicomMediaList;
   }
 })(typeof global !== "undefined" ? global : void 0);
--- js/script.js
+++ js/script.js
@@ -35,7 +35,7 @@
                 this._currentMusic = 0;
             }
 
-            $(document).trigger('cmp-music-end', [this._currentMusic, error]);
+            $(document).trigger('cmp-media-end', [this._currentMusic, error]);
 
             this._makePlayer();
         }
@@ -144,24 +144,190 @@
         }
     }
 
+    class CeicomVideoPlayer {
+        constructor(videos, options = {}) {
+            this._player = {};
+            this._videos = videos;
+            this.options = options;
+            this._actualVideo = 0;
+            this._isPlaying = false;
+            this._fakePlayer = $('.cmp-player');
+            this._$playBtn = this._fakePlayer.find('.play-btn');
+            this._$progressBar = this._fakePlayer.find('.progress-bar');
+            this._$time = this._fakePlayer.find('.time');
+            this._$volumeWrapper = this._fakePlayer.find('.volume-wrapper');
+            this._$volumeInput = this._fakePlayer.find('.volume-input');
+            this._$expandBtn = this._fakePlayer.find('.expand-button');
+
+            this.init();
+        }
+
+        init() {
+            if (!this._videos.length) {
+                $(this._fakePlayer).html('<span class="error-msg">Nenhuma música detectada na lista atual.</span>');
+                return;
+            }
+
+            const tag = document.createElement('script');
+            const firstScriptTag = document.getElementsByTagName('script')[0];
+
+            tag.src = 'https://www.youtube.com/iframe_api';
+            firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+            window.onYouTubeIframeAPIReady = () => {
+                // https://developers.google.com/youtube/player_parameters#Parameters
+                const options = {
+                    width: $('body').width(),
+                    height: 600,
+                    playerVars: {
+                        autoplay: 0,
+                        cc_load_policy: 0,
+                        controls: 0,
+                        disablekb: 1,
+                        enablejsapi: 1,
+                        iv_load_policy: 3,
+                        modestbranding: 1,
+                        rel: 0,
+                        showinfo: 0
+                    },
+                    events: {
+                        onReady: onPlayerReady,
+                        onStateChange: onPlayerStateChange,
+                        onError: onPlayerError
+                    },
+                    ...this.options
+                };
+
+                this._player = new YT.Player('cvp-player', options);
+                this._initFakePlayerActions();
+            }
+
+            const onPlayerReady = () => {
+                const videoIds = this._videos.map(item => this._validaYoutube(item.link));
+                this._player.loadPlaylist(videoIds);
+                this._player.setLoop(true);
+                this._isPlaying = true;
+
+                setInterval(() => {
+                    this._updateTime(this._player.getCurrentTime(), this._player.getDuration());
+                    this._updateProgressBar(this._player.getCurrentTime(), this._player.getDuration());
+                }, 1000);
+            }
+
+            const onPlayerStateChange = event => {
+
+                // Play/Pause Video
+                this._isPlaying = true;
+                this._fakePlayer.addClass('playing');
+                if (event.data === 2) {
+                    this._isPlaying = false;
+                    this._fakePlayer.removeClass('playing');
+                }
+
+                // Verifica se foi alterado o video
+                if (this._player.getPlaylistIndex() !== this._actualVideo) {
+                    this._actualVideo = this._player.getPlaylistIndex();
+                    this._videoChange();
+                }
+            }
+
+            const onPlayerError = () => {
+                this._videoChange(true);
+                this._player.nextVideo();
+            }
+        }
+
+        _initFakePlayerActions() {
+            this._$playBtn.on('click', () => this._isPlaying ? this._player.pauseVideo() : this._player.playVideo());
+
+            this._$volumeWrapper.on('click', e => {
+                if (!$(e.target).hasClass('volume-wrapper')) return;
+                this._$volumeWrapper.toggleClass('active');
+            });
+
+            this._$volumeInput.on('input', () => this.setVolume(this._$volumeInput.val() / 10));
+
+            this._$expandBtn.on('click', () => {
+                const ele = this._fakePlayer.parent()[0];
+                if (document.fullscreen || document.fullscreenElement || document.mozFullScreenElement || document.webkitFullscreenElement || document.msFullscreenElement) {
+                    if (document.exitFullscreen)
+                        document.exitFullscreen();
+                    else if (document.msExitFullscreen)
+                        document.msExitFullscreen();
+                    else if (document.mozCancelFullScreen)
+                        document.mozCancelFullScreen();
+                    else if (ele.webkitExitFullscreen)
+                        document.webkitExitFullscreen();
+                    return;
+                }
+
+                if (ele.requestFullscreen)
+                    ele.requestFullscreen();
+                else if (ele.msRequestFullscreen)
+                    ele.msRequestFullscreen();
+                else if (ele.mozRequestFullScreen)
+                    ele.mozRequestFullScreen();
+                else if (ele.webkitRequestFullscreen)
+                    ele.webkitRequestFullscreen();
+            });
+        }
+
+        getVolume() {
+            return this._player.getVolume();
+        }
+
+        setVolume(value) {
+            this._player.setVolume(value * 100);
+        }
+
+        _validaYoutube(link) {
+            const regex = /(?:youtube\.com\/\S*(?:(?:\/e(?:mbed))?\/|watch\?(?:\S*?&?v\=))|youtu\.be\/)([a-zA-Z0-9_-]{6,11})/g;
+            const match = regex.exec(link);
+
+            return match && match[1].length === 11 ? match[1] : false;
+        };
+
+        _videoChange(error) {
+            $(document).trigger('cmp-media-end', [this._player.getPlaylistIndex(), error]);
+        }
+
+        _updateProgressBar(currentTime, musicDuration) {
+            this._$progressBar.css('transform', `scaleX(${Math.ceil(currentTime) / Math.ceil(musicDuration)})`);
+        }
+
+        _updateTime(currentTime, musicDuration) {
+            const extraZero = n => n < 10 ? '0' + n : n;
+
+            const formatedTime = time => {
+                const s = parseInt(Math.ceil(time) % 60);
+                const m = parseInt((Math.ceil(time) / 60) % 60);
+
+                return extraZero(m) + ':' + extraZero(s);
+            }
+
+            this._$time.text(formatedTime(currentTime) + ' / ' + formatedTime(musicDuration));
+        }
+    }
+
     class CeicomMediaList {
-        constructor(musics) {
-            this._musics = musics;
+        constructor(mediaList, isVideo = false) {
+            this._mediaList = mediaList;
+            this.isVideo = isVideo;
             this._wrapper = $('.cml-music-list');
 
             this.init();
         }
 
         init() {
-            if (!this._musics.length) return;
+            if (!this._mediaList.length) return;
 
-            const html = this._musics.map((item, index) => {
+            const html = this._mediaList.map((item, index) => {
                 if (item.typemedia === 'comercial' || item.typemedia === 'vinheta') return;
-                return `<li class="music-item ${index === 0 ? 'active' : ''}" data-music="${index}">${item.track}</li>`;
+                return `<li class="music-item ${index === 0 ? 'active' : ''}" 
+                            data-music="${index}">${this.isVideo ? item.title : item.track}</li>`;
             }).join('');
 
-            $(document).on('cmp-music-end', (e, index, error) => {
-
+            $(document).on('cmp-media-end', (e, index, error) => {
                 if (error) this._wrapper.find('.music-item.active').addClass('error');
 
                 this._wrapper.find('.music-item').removeClass('active');
@@ -191,9 +357,11 @@
     // commonjs
     if (typeof exports !== "undefined") {
         exports.CeicomMediaPlayer = CeicomMediaPlayer;
+        exports.CeicomVideoPlayer = CeicomVideoPlayer;
         exports.CeicomMediaList = CeicomMediaList;
     } else {
         window.CeicomMediaPlayer = CeicomMediaPlayer;
+        window.CeicomVideoPlayer = CeicomVideoPlayer;
         window.CeicomMediaList = CeicomMediaList;
     }
 
